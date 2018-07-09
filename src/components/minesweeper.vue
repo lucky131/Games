7a5cbb63b77376e0a21dc69af11839a36891cbb1
liyuhao <542723049@qq.com>
2018-07-09 19:02:58 +0800

    自定义开局

--- src/components/minesweeper.vue
+++ src/components/minesweeper.vue
@@ -11,6 +11,41 @@
       </div>
     </div>
     <div>{{flagNumber}} / {{config.mines}}</div>
+    <el-dialog
+      title="新游戏"
+      :visible.sync="isDialogShow"
+      width="600px"
+      :show-close="falseData"
+      :close-on-click-modal="falseData"
+      :close-on-press-escape="falseData">
+      <el-form label-width="75px">
+        <el-form-item label="格子大小">
+          <el-input-number v-model="config.blockSize" :min="10" :step="5"></el-input-number>
+        </el-form-item>
+        <el-form-item label="难度">
+          <el-radio-group v-model="config.difficulty">
+            <el-radio :label="1">简单</el-radio>
+            <el-radio :label="2">中等</el-radio>
+            <el-radio :label="3">困难</el-radio>
+            <el-radio :label="4">自定义</el-radio>
+          </el-radio-group>
+          <div>
+            <div class="dialogRow">
+              宽：<el-input-number :disabled="config.difficulty!==4" v-model="config.width" :min="10"></el-input-number>
+            </div>
+            <div class="dialogRow">
+              高：<el-input-number :disabled="config.difficulty!==4" v-model="config.height" :min="10"></el-input-number>
+            </div>
+            <div class="dialogRow">
+              雷：<el-input-number :disabled="config.difficulty!==4" v-model="config.mines" :min="1"></el-input-number>
+            </div>
+          </div>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="">
+        <el-button type="primary" @click="start()">开始</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -49,6 +84,9 @@
         }
       }
     }
+    .dialogRow{
+      margin: 10px 0;
+    }
   }
 </style>
 
@@ -57,8 +95,11 @@
     name: 'minesweeper',
     data () {
       return {
+        falseData: false,
+        isDialogShow: true,
         config: {
           blockSize: 40,
+          difficulty: 1,
           width: 10,
           height: 10,
           mines: 10
@@ -75,36 +116,38 @@
     },
     mounted: function () {
       let that = this;
-
-      //调整雷区的宽高
-      $("#minefield").css("width", this.config.width * this.config.blockSize + "px")
-        .css("height", this.config.height * this.config.blockSize + "px");
-
-      //生成blocks
-      this.blocks = [];
-      for(let i=0;i<this.config.height;i++){
-        for(let j=0;j<this.config.width;j++){
-          this.blocks.push({
-            index: i*this.config.width+j,
-            row: i,
-            col: j,
-            mine: 0,
-            number: 0,
-            open: false,
-            flag: false,
-          });
-        }
-      }
-
-      this.$nextTick(() => {
-        $(".block").css("width", this.config.blockSize + "px")
-          .css("height", this.config.blockSize + "px")
-          .css("line-height", this.config.blockSize + "px");
-      });
-
-      //this.generate();
     },
     methods: {
+      start(){
+        this.isDialogShow = false;
+        this.isGenerate = false;
+
+        //调整雷区的宽高
+        $("#minefield").css("width", this.config.width * this.config.blockSize + "px")
+          .css("height", this.config.height * this.config.blockSize + "px");
+
+        //生成blocks
+        this.blocks = [];
+        for(let i=0;i<this.config.height;i++){
+          for(let j=0;j<this.config.width;j++){
+            this.blocks.push({
+              index: i*this.config.width+j,
+              row: i,
+              col: j,
+              mine: 0,
+              number: 0,
+              open: false,
+              flag: false,
+            });
+          }
+        }
+
+        this.$nextTick(() => {
+          $(".block").css("width", this.config.blockSize + "px")
+            .css("height", this.config.blockSize + "px")
+            .css("line-height", this.config.blockSize + "px");
+        });
+      },
       generate(safeIndex){
         let that = this;
         do{
@@ -198,7 +241,11 @@
             this.open(i+1, j+1);
           }
           if(this.isWin()){
-            this.$alert("u win! ");
+            this.$alert("u win! ", {
+              callback: () => {
+                this.isDialogShow = true;
+              }
+            });
           }
         }
       },
@@ -247,6 +294,23 @@
         if(this.isGenerate && !this.blocks[index].open){
           this.flagNumber += this.blocks[index].flag ? -1 : 1;
           this.blocks[index].flag = !this.blocks[index].flag;
+        }
+      }
+    },
+    watch: {
+      "config.difficulty": function (val, oldVal) {
+        if(val===1){
+          this.config.width=10;
+          this.config.height=10;
+          this.config.mines=10;
+        } else if(val===2){
+          this.config.width=16;
+          this.config.height=16;
+          this.config.mines=40;
+        } else if(val===3){
+          this.config.width=30;
+          this.config.height=16;
+          this.config.mines=99;
         }
       }
     }
