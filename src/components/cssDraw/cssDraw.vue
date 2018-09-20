4f4830bcf33a33f83b7198104b253be00ef7804f
liyuhao <542723049@qq.com>
2018-09-20 13:35:19 +0800

    时钟

--- src/components/cssDraw/cssDraw.vue
+++ src/components/cssDraw/cssDraw.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="wrap">
-    <div class="page">
+    <div class="page" style="background-color: #888">
       <div class="rainy"></div>
     </div>
-    <div class="page">
+    <div class="page" style="background-color: white">
+      <div class="rainbow"></div>
+    </div>
+    <div class="page" style="background-color: #4fb8ff">
+      <div class="clock">
+        <div class="tick top"></div>
+        <div class="tick right"></div>
+        <div class="tick bottom"></div>
+        <div class="tick left"></div>
+        <div class="needle hour"></div>
+        <div class="needle minute"></div>
+        <div class="needle second"></div>
+        <div class="center"></div>
+        <div class="shadow"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -19,7 +33,6 @@
     .page{
       width: 100%;
       height: 100vh;
-      background-color: #888;
       display: flex;
       flex-flow: row nowrap;
       justify-content: center;
@@ -70,7 +83,7 @@
           position: absolute;
           top: 10vw;
           left: 8vw;
-          animation: rainy3 0.75s linear infinite;
+          animation: rainy3 0.5s linear infinite;
           @keyframes rainy3 {
             0% {
               box-shadow:
@@ -163,6 +176,128 @@
           }
         }
       }
+      .rainbow{
+        width: 20vw;
+        height: 20vw;
+        border-radius: 100% 0 0 0;
+        background-color: transparent;
+        position: relative;
+        transform: rotate(50deg);
+        box-shadow:
+          -0.5vw -0.5vw 0 0.5vw #a900ff,
+          -1.0vw -1.0vw 0 1.0vw #0a00ff,
+          -1.5vw -1.5vw 0 1.5vw #00dbff,
+          -2.0vw -2.0vw 0 2.0vw #00ff1d,
+          -2.5vw -2.5vw 0 2.5vw #fdff00,
+          -3.0vw -3.0vw 0 3.0vw #ff9000,
+          -3.5vw -3.5vw 0 3.5vw #ff0400;
+        animation: rainbow1 5s ease-in-out infinite;
+        @keyframes rainbow1 {
+          50% { transform: rotate(40deg); }
+        }
+        &:before{
+          content: '';
+          width: 30vw;
+          height: 5vw;
+          border-radius: 50%;
+          background-color: black;
+          opacity: 0.2;
+          position: absolute;
+          top: 18vw;
+          left: 8vw;
+          transform: rotate(-50deg);
+          transform-origin: 50% 50%;
+          animation: rainbow2 5s ease-in-out infinite;
+          @keyframes rainbow2 {
+            50% {
+              transform: scale(0.75) rotate(-40deg) translateX(-3vw);
+              opacity: 0.1;
+            }
+          }
+        }
+      }
+      .clock{
+        width: 30vw;
+        height: 30vw;
+        border-radius: 50%;
+        background-color: white;
+        box-shadow: 0 0 0 2vw #97dfff;
+        position: relative;
+        .tick{
+          width: 0.4vw;
+          height: 2vw;
+          background-color: #666;
+          position: absolute;
+          top: 1vw;
+          left: 14.8vw;
+          z-index: 1;
+          transform-origin: 50% 14vw;
+          &.top{transform: rotate(0deg)}
+          &.right{transform: rotate(90deg)}
+          &.bottom{transform: rotate(180deg)}
+          &.left{transform: rotate(270deg)}
+        }
+        .needle{
+          position: absolute;
+          transform-origin: 50% 100%;
+          animation: needle linear infinite;
+          @keyframes needle {
+            100% {
+              transform: rotate(360deg);
+            }
+          }
+          &.hour{
+            width: 0.8vw;
+            height: 8vw;
+            border-radius: 0.4vw 0.4vw 0 0;
+            background-color: #999;
+            top: 7vw;
+            left: 14.6vw;
+            z-index: 1;
+            animation-duration: 43200s;
+          }
+          &.minute{
+            width: 0.6vw;
+            height: 12vw;
+            border-radius: 0.3vw 0.3vw 0 0;
+            background-color: #999;
+            top: 3vw;
+            left: 14.7vw;
+            z-index: 2;
+            animation-duration: 3600s;
+          }
+          &.second{
+            width: 0.2vw;
+            height: 12vw;
+            border-radius: 0.1vw 0.1vw 0 0;
+            background-color: red;
+            top: 3vw;
+            left: 14.9vw;
+            z-index: 3;
+            animation-duration: 60s;
+          }
+        }
+        .center{
+          width: 2vw;
+          height: 2vw;
+          border-radius: 50%;
+          background-color: #666;
+          position: absolute;
+          top: 14vw;
+          left: 14vw;
+          z-index: 4;
+        }
+        .shadow{
+          width: 34vw;
+          height: 34vw;
+          position: absolute;
+          top: -2vw;
+          left: -2vw;
+          z-index: 5;
+          background: linear-gradient(90deg, rgba(0,0,0,.339), rgba(0,0,0,.134), rgba(0,0,0,.032), rgba(0,0,0,0));
+          transform: rotate(45deg) translateX(17vw);
+        }
+      }
     }
   }
 </style>
@@ -176,7 +311,12 @@
       }
     },
     mounted(){
-
+      let date = new Date();
+      let h = date.getHours();
+      let m = date.getMinutes();
+      let s = date.getSeconds();
+      let totalS = h*60*60 + m*60 + s;
+      $(".needle").css("animation-delay", "-"+totalS+"s");
     },
     methods: {
 
