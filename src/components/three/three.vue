d85d2fbe85da743e6ef11fd332779914097b8ea0
lucky131baitao <542723049@qq.com>
2019-04-11 17:37:37 +0800

    地形编辑器初版 只能增加方块

--- src/components/three/three.vue
+++ src/components/three/three.vue
@@ -1,6 +1,25 @@
 <template>
   <div class="wrap">
     <div id="container"></div>
+    <div class="controller">
+      <el-form label-width="100px">
+        <el-form-item label="模式：">
+          <el-radio-group v-model="controller.editStatus">
+            <el-radio label="add">新增</el-radio>
+            <el-radio label="remove">移除</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item v-if="controller.editStatus==='add'" label="颜色：">
+          <el-color-picker v-model="controller.color"></el-color-picker>
+        </el-form-item>
+        <el-form-item label="辅助线：">
+          <el-radio-group v-model="controller.axesHelper" @change="onHelperChange">
+            <el-radio :label="true">开</el-radio>
+            <el-radio :label="false">关</el-radio>
+          </el-radio-group>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
@@ -8,19 +27,20 @@
   .wrap{
     width: 100vw;
     height: 100vh;
+    background-color: black;
     display: flex;
     flex-flow: row nowrap;
     justify-content: center;
     align-items: center;
     #container{
-      width: 1000px;
+      width: 800px;
       height: 800px;
-      position: relative;
-      .dg.ac{
-        position: absolute;
-        top: 0;
-        right: 0;
-      }
+    }
+    .controller{
+      width: 400px;
+      height: 800px;
+      background-color: white;
+      border-left: 1px solid black;
     }
   }
 </style>
@@ -34,59 +54,225 @@
     name: "three",
     data(){
       return{
+        config: {
+          blockSize: 4,
+          selectedBlockColor: "#5e64dd",
+        },
+        controller: {
+          editStatus: "add",
+          color: "#ffc06c",
+          axesHelper: true,
+        },
+        blocks: {},
+        selectedBlock: null,
         res: {},
         camera: null,
+        cameraController: null,
+        clock: null,
         scene: null,
+        axesHelper: null,
         renderer: null,
         stats: null,
-        gui: null,
-        controls: null,
+        mouse: null,
+        lastMouse: null,
+        raycaster: null,
       }
     },
     mounted(){
+      window.THREE = require("three");
+      require("../magicCube/lib/OrbitControls");
+
       this.loadRes();
       this.init();
       this.initStats();
-      this.initGui();
       this.animate();
     },
     methods: {
+      //加载资源
       loadRes(){
 
       },
+      //初始化
       init(){
         let container = document.getElementById('container');
 
         //scene
         this.scene = new THREE.Scene();
-        this.scene.fog = new THREE.FogExp2("#ccc", 0.005);
-        this.scene.add(new THREE.AxesHelper(50));
+
+        //axesHelper
+        this.axesHelper = new THREE.AxesHelper(1000);
+        this.scene.add(this.axesHelper);
 
         //camera
-        // this.camera = new THREE.PerspectiveCamera(50, 1000/800, 0.1, 1000);
-        this.camera = new THREE.OrthographicCamera(-50,50,40,-40, 0.01, 1000);
-        this.camera.position.set(10,10,10);
+        this.camera = new THREE.PerspectiveCamera(50, 800/800, 0.1, 1000);
+        // this.camera = new THREE.OrthographicCamera(-50,50,50,-50, 0.01, 1000);
+        this.camera.position.set(100,100,100);
         this.camera.lookAt(0,0,0);
+        this.camera.position.y = 120;
+        this.cameraController = new THREE.OrbitControls(this.camera, container);
+        this.clock = new THREE.Clock();
 
         //render
         this.renderer = new THREE.WebGLRenderer({antialias: true}); //抗锯齿
         this.renderer.setSize(container.clientWidth, container.clientHeight);
-        this.renderer.setClearColor("#ccc");
-        this.renderer.shadowMap.enabled = true;
+        this.renderer.setClearColor("#fff");
 
         //light
-        let ambientLight = new THREE.AmbientLight("#fff");
+        let ambientLight = new THREE.AmbientLight("#999");
         this.scene.add(ambientLight);
+        let directionalLight1 = new THREE.DirectionalLight("#fff", 1);
+        directionalLight1.position.set(3,5,5);
+        this.scene.add(directionalLight1);
+        let directionalLight2 = new THREE.DirectionalLight("#fff", 0.5);
+        directionalLight2.position.set(-5,5,-3);
+        this.scene.add(directionalLight2);
 
-        //cube
-        let cube = new THREE.Mesh(
-          new THREE.BoxGeometry(2,2,2),
-          new THREE.MeshPhongMaterial({color: "#00f"})
-        );
-        cube.position.set(0,0,0);
-        this.scene.add(cube);
+        //plane
+        this.createBlockZone(-5,-1,-5, 5,-1,5, "ground", "#5fcc5a");
+        this.createAllTransparentBlock();
 
         container.appendChild(this.renderer.domElement);
+
+        //mouse
+        this.mouse = new THREE.Vector2();
+        this.lastMouse = new THREE.Vector2();
+
+        //raycaster
+        this.raycaster = new THREE.Raycaster();
+
+        //event
+        container.addEventListener("mousemove", (event) => {
+          this.mouse.x = (event.offsetX/event.target.width)*2-1;
+          this.mouse.y = -(event.offsetY/event.target.height)*2+1;
+          this.raycaster.setFromCamera(this.mouse, this.camera);
+          let status = this.controller.editStatus;
+          switch (status) {
+            case "add":
+              let intersects = this.raycaster.intersectObjects(this.scene.children).filter(n => n.object.type === "transparent");
+              if(intersects.length > 0){
+                if(this.selectedBlock){
+                  this.selectedBlock.material.opacity = 0;
+                }
+                this.selectedBlock = intersects[0].object;
+                this.selectedBlock.material.opacity = 0.5;
+              } else {
+                if(this.selectedBlock){
+                  this.selectedBlock.material.opacity = 0;
+                  this.selectedBlock = null;
+                }
+              }
+              break;
+            case "remove":
+              break;
+          }
+        }, false);
+        container.addEventListener("mousedown", (event) => {
+          this.lastMouse.x = (event.offsetX/event.target.width)*2-1;
+          this.lastMouse.y = -(event.offsetY/event.target.height)*2+1;
+        }, false);
+        container.addEventListener("mouseup", (event) => {
+          this.mouse.x = (event.offsetX/event.target.width)*2-1;
+          this.mouse.y = -(event.offsetY/event.target.height)*2+1;
+          if(this.mouse.x === this.lastMouse.x && this.mouse.y === this.lastMouse.y){
+            let status = this.controller.editStatus;
+            switch (status) {
+              case "add":
+                if(this.selectedBlock){
+                  let pp = this.selectedBlock.xyz;
+                  this.scene.remove(this.selectedBlock);
+                  delete this.blocks[`${pp.x},${pp.y},${pp.z}`];
+                  this.selectedBlock = null;
+                  this.createBlock(pp.x,pp.y,pp.z, "block", this.controller.color);
+                  this.clearTransparentBlock();
+                  this.createAllTransparentBlock();
+                }
+                break;
+              case "remove":
+                break;
+            }
+          }
+        }, false);
+      },
+      createBlockZone(x1,y1,z1, x2,y2,z2, type, color){
+        let x_min = Math.min(x1, x2),
+          x_max = Math.max(x1, x2),
+          y_min = Math.min(y1, y2),
+          y_max = Math.max(y1, y2),
+          z_min = Math.min(z1, z2),
+          z_max = Math.max(z1, z2);
+        for(let x = x_min; x <= x_max; x++){
+          for(let y = y_min; y <= y_max; y++){
+            for(let z = z_min; z <= z_max; z++){
+              this.createBlock(x,y,z, type, color);
+            }
+          }
+        }
+      },
+      createBlock(x,y,z, type, color){
+        let material;
+        if(type === "transparent")
+          color = this.config.selectedBlockColor;
+        let block = new THREE.Mesh(
+          new THREE.BoxGeometry(this.config.blockSize, this.config.blockSize, this.config.blockSize),
+          new THREE.MeshLambertMaterial({color: color})
+        );
+        let pp = this.xyz2PhysicalPosition(x,y,z);
+        block.position.set(pp.x, pp.y, pp.z);
+        block.type = type;
+        block.xyz = {x,y,z};
+        if(type === "transparent"){
+          block.material.transparent = true;
+          block.material.opacity = 0;
+        }
+        this.blocks[`${x},${y},${z}`] = block;
+        this.scene.add(block);
+      },
+      //逻辑坐标 -> 物理坐标
+      xyz2PhysicalPosition(x,y,z){
+        return {
+          x: x * this.config.blockSize,
+          y: y * this.config.blockSize,
+          z: z * this.config.blockSize
+        }
+      },
+      createAllTransparentBlock(){
+        for(let key in this.blocks){
+          let block = this.blocks[key];
+          let type = block.type;
+          //              0 x+   1 x-   2 y+   3 y-   4 z+   5 z-
+          let position = [false, false, false, false, false, false];
+          switch (type) {
+            case "ground":
+              position[2] = true;
+              break;
+            case "block":
+              position = [true, true, true, true, true, true];
+              break;
+          }
+          if(position[0]) this.createTransparentBlock(block.xyz.x+1, block.xyz.y, block.xyz.z);
+          if(position[1]) this.createTransparentBlock(block.xyz.x-1, block.xyz.y, block.xyz.z);
+          if(position[2]) this.createTransparentBlock(block.xyz.x, block.xyz.y+1, block.xyz.z);
+          if(position[3]) this.createTransparentBlock(block.xyz.x, block.xyz.y-1, block.xyz.z);
+          if(position[4]) this.createTransparentBlock(block.xyz.x, block.xyz.y, block.xyz.z+1);
+          if(position[5]) this.createTransparentBlock(block.xyz.x, block.xyz.y, block.xyz.z-1);
+        }
+      },
+      createTransparentBlock(x,y,z){
+        if(this.blocks[`${x},${y},${z}`]) return;
+        this.createBlock(x,y,z, "transparent");
+      },
+      clearTransparentBlock(){
+        for(let key in this.blocks){
+          let block = this.blocks[key];
+          if(block.type === "transparent"){
+            this.scene.remove(block);
+            delete this.blocks[key];
+          }
+        }
+      },
+      refreshTransparentBlock(){
+        this.clearTransparentBlock();
+        this.createAllTransparentBlock();
       },
       initStats(){
         this.stats = new Stats();
@@ -97,25 +283,19 @@
         // document.getElementById("container").appendChild(this.stats.domElement);
         document.body.appendChild(this.stats.domElement);
       },
-      initGui(){
-        //防止vue重新渲染导致多个gui组件
-        let olds = document.getElementsByClassName("dg main a");
-        for(let i = 0; i<olds.length; i++){
-          olds[i].parentElement.removeChild(olds[i]);
-        }
-
-        this.gui = new dat.GUI({width: 400});
-        this.controls = {
-
-        };
-        // this.gui.add(this.controls, "blockOfMagicCube", 3, 5, 1);
-      },
       animate(){
         //stats
         this.stats.update();
 
+        //trackballControls
+        let delta = this.clock.getDelta(); //距离上一帧的秒数
+        this.cameraController.update(delta);
+
         requestAnimationFrame(this.animate);
         this.renderer.render(this.scene, this.camera);
+      },
+      onHelperChange(v){
+        this.axesHelper.visible = v;
       },
     }
   }
