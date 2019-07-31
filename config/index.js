80b71ff644210eae1bab6e42e2cf1ee7fc10ad2b
lucky131baitao <542723049@qq.com>
2019-07-31 09:26:59 +0800

    host修改

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    // host: '172.16.6.13', // can be overwritten by process.env.HOST
+    // host: '172.16.6.15', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: true,
     errorOverlay: true,
