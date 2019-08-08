524bdbcc5867abadacdabb4643fef6bf8ff73cd4
lucky131baitao <542723049@qq.com>
2019-08-08 16:29:44 +0800

    黑暗森林暂存

--- src/components/darkForest/darkForest.vue
+++ src/components/darkForest/darkForest.vue
@@ -1,12 +1,24 @@
 <template>
   <div class="darkForest">
-    darkForest
+    <div class="game">
+      123
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
   .darkForest{
-
+    width: 100vw;
+    height: 100vh;
+    background-color: black;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    .game{
+      width: 800px;
+      height: 600px;
+      background-color: white;
+    }
   }
 </style>
 
