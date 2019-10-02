ae1d71c4031791fdf061cdaa960b83046194de36
lucky131 <542723049@qq.com>
2019-10-02 12:27:55 +0800

    实现家国梦底部tab的效果

--- src/components/darkForest/darkForest.vue
+++ src/components/darkForest/darkForest.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="darkForest">
-    <div class="game">
-      123
+    <div class="game"></div>
+    <div class="tabs">
+      <div class="tab" :style="{width: screenWidth/5*[2,1,1,1][tabIndex]+'px'}" @click="tabIndex = 0">1</div>
+      <div class="tab" :style="{width: screenWidth/5*[1,2,1,1][tabIndex]+'px'}" @click="tabIndex = 1">2</div>
+      <div class="tab" :style="{width: screenWidth/5*[1,1,2,1][tabIndex]+'px'}" @click="tabIndex = 2">3</div>
+      <div class="tab" :style="{width: screenWidth/5*[1,1,1,2][tabIndex]+'px'}" @click="tabIndex = 3">4</div>
+      <div class="bg" :style="{width: screenWidth/5*2+'px', left: screenWidth/5*tabIndex+'px'}"></div>
     </div>
   </div>
 </template>
@@ -12,12 +17,31 @@
     height: 100vh;
     background-color: black;
     display: flex;
-    justify-content: center;
-    align-items: center;
+    flex-flow: column nowrap;
     .game{
-      width: 800px;
-      height: 600px;
-      background-color: white;
+      width: 100%;
+      flex: 1 0 0;
+    }
+    .tabs{
+      width: 100%;
+      height: 100px;
+      display: flex;
+      background-color: #2e7bff;
+      position: relative;
+      .tab{
+        height: 100px;
+        line-height: 100px;
+        text-align: center;
+        transition: width 300ms;
+        z-index: 1;
+      }
+      .bg{
+        height: 100px;
+        background-color: chartreuse;
+        position: absolute;
+        top: 0;
+        transition: left 300ms;
+      }
     }
   }
 </style>
@@ -27,7 +51,12 @@ export default {
   name: 'darkForest',
   data () {
     return {
+      screenWidth: 0,
+      tabIndex: 0
     }
+  },
+  created () {
+    this.screenWidth = window.innerWidth
   },
   mounted () {
   },
