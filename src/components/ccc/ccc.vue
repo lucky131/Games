327856530f66eaac3977647b09d39431279e6509
lucky131 <542723049@qq.com>
2020-03-05 18:44:27 +0800

    ccc1.4

--- src/components/ccc/ccc.vue
+++ src/components/ccc/ccc.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="wrap">
-    <div class="title">Little Tools by -3- v1.3
+    <div class="title">Little Tools by -3- v1.4
     </div>
     <el-tabs type="border-card">
       <el-tab-pane label="1">
@@ -50,13 +50,29 @@
         </div>
         <div id="result3" class="result"></div>
       </el-tab-pane>
+
+      <el-tab-pane label="4">
+        <el-input
+          type="textarea"
+          :rows="10"
+          resize="none"
+          placeholder="请粘贴html：div.tbody"
+          v-model="inputHtml4">
+        </el-input>
+        <div class="opeBtns">
+          <el-button type="primary" icon="el-icon-refresh" @click="transform4()">转换</el-button>
+          <el-button type="default" icon="el-icon-close" @click="clear4()">清空</el-button>
+          <el-button v-if="isCopyBtn4Show" class="copyBtn" type="success" icon="el-icon-share" data-clipboard-target="#result4">复制结果</el-button>
+        </div>
+        <div id="result4" class="result"></div>
+      </el-tab-pane>
     </el-tabs>
   </div>
 </template>
 
 <style scoped lang="scss">
   .wrap{
-    width: 1000px;
+    width: 1200px;
     margin: 0 auto;
     text-align: center;
     .title{
@@ -72,6 +88,7 @@
     .result{
       width: 100%;
       margin-top: 40px;
+      font-size: 12px;
       /deep/ table{
         width: 100%;
       }
@@ -93,6 +110,8 @@
         isCopyBtn2Show: false,
         input3: "",
         isCopyBtn3Show: false,
+        inputHtml4: "",
+        isCopyBtn4Show: false,
       }
     },
     mounted(){
@@ -225,6 +244,50 @@
         this.input3 = "";
         this.isCopyBtn3Show = false;
         $("#result3").empty();
+      },
+      transform4(){
+        if(this.inputHtml4.length === 0){
+          this.$message({
+            message: '内容不能为空',
+            type: 'error'
+          });
+        } else {
+          let dom = $("<div></div>").append($(this.inputHtml4));
+          if(dom.find("div.tbody").length === 0){
+            this.$message({
+              message: '找不到div.tbody',
+              type: 'error'
+            });
+          } else {
+            let tbodyDom = dom.find("div.tbody");
+            let appendTableDom = $("<table border='1' cellpadding='3' cellspacing='0'></table>");
+            let rowNum = tbodyDom[0].childElementCount;
+            for(let i = 0; i < rowNum; i++){
+              let rowDom = tbodyDom.children("a.table-row").eq(i).children("div.g-row").eq(0); //默认3个子元素 姓名 国籍 剩余
+              let appendTrDom = $("<tr></tr>");
+              appendTrDom.append(`<td>${rowDom.children().eq(0).text()}</td>`);
+              appendTrDom.append(`<td>${rowDom.children().eq(1).text()}</td>`);
+              let otherRowDom = rowDom.children().eq(2);
+              let colNum = otherRowDom.children().length;
+              for(let j = 0; j < colNum; j++){
+                let cellDom = otherRowDom.children().eq(j);
+                let text1 = cellDom.children().eq(3).text();
+                let text2 = cellDom.children().eq(5).text();
+                appendTrDom.append(`<td>${text1}</td>`);
+                appendTrDom.append(`<td>${text2}</td>`);
+              }
+              appendTableDom.append(appendTrDom);
+            }
+            $("#result4").empty();
+            $("#result4").append(appendTableDom);
+            this.isCopyBtn4Show = true;
+          }
+        }
+      },
+      clear4(){
+        this.inputHtml4 = "";
+        this.isCopyBtn4Show = false;
+        $("#result4").empty();
       }
     }
   }
