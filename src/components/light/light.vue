e52ad6d22c67d479181826dd3cef29c52078cb18
lucky131 <542723049@qq.com>
2020-08-21 16:55:36 +0800

    light 3*3基本完成

---
+++ src/components/light/light.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
+
+<script>
+  export default {
+    name: "light",
+    props: {
+
+    },
+    data(){
+      return{
+
+      }
+    },
+    mounted(){
+
+    },
+    methods: {
+
+    }
+  }
+</script>
